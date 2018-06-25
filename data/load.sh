760ee699a5458684ed04e1d9eebf7729c91ee766
Frederick Robinson <frederick.robinson@gmail.com>
2018-06-24 17:18:45 -0700

    db build

--- data/load.sh
+++ data/load.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 python generate.py > data.csv
+rm data.db
+sqlite3 data.db <<EOF 
+create table salaries(idnumber integer,
+                          name string,
+                               salary integer,
+                               department string,
+                               join_date date);
+.mode csv
+.import data.csv salaries
+.quit
+EOF
